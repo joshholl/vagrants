c7e1a7a31e610ecd8665ed05907dedef1138d790
Josh Hollandsworth <josh.hollandsworth@gmail.com>
2016-10-27 18:06:08 -0500

    corrected additions installation

--- centos7/scripts/install_additions.sh
+++ centos7/scripts/install_additions.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+if [ ! -f /tmp/VBoxGuestAdditions.iso ]
+then
+  echo "Unable to find additions....exiting"
+  exit 1
+fi
+
 # Mount the disk image
 cd /tmp
 sudo mkdir /tmp/isomount
-sudo mount -t iso9660 -o loop /root/VBoxGuestAdditions.iso /tmp/isomount
+sudo mount -t iso9660 -o loop /tmp/VBoxGuestAdditions.iso /tmp/isomount
 
 # Install the drivers
 sudo /tmp/isomount/VBoxLinuxAdditions.run
