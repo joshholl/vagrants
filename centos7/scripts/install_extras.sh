4f95a1e1ea42a807c4a1e52d0119958921991373
Josh Hollandsworth <josh.hollandsworth@gmail.com>
2016-10-26 22:39:36 -0500

    made extras pull from dropbox

--- centos7/scripts/install_extras.sh
+++ centos7/scripts/install_extras.sh
@@ -11,20 +11,19 @@ fi
 
 if [ ! -f $EXTRAS/oracle-instantclient12.1-basic-12.1.0.2.0-1.x86_64.rpm ]
 then
-	wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn/linux/instantclient/121020/oracle-instantclient12.1-basic-12.1.0.2.0-1.x86_64.rpm"
+  wget https://dl.dropboxusercontent.com/u/18038046/ora/oracle-instantclient12.1-basic-12.1.0.2.0-1.x86_64.rpm 
 fi
 
 if [ ! -f $EXTRAS/oracle-instantclient12.1-devel-12.1.0.2.0-1.x86_64.rpm ]
 then
-	wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn/linux/instantclient/121020/oracle-instantclient12.1-devel-12.1.0.2.0-1.x86_64.rpm"
+ wget https://dl.dropboxusercontent.com/u/18038046/ora/oracle-instantclient12.1-devel-12.1.0.2.0-1.x86_64.rpm
 fi
 
 
 if [ ! -f $EXTRAS/oracle-instantclient12.1-sqlplus-12.1.0.2.0-1.x86_64.rpm ]
 then
-	wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn/linux/instantclient/121020/oracle-instantclient12.1-sqlplus-12.1.0.2.0-1.x86_64.rpm"
+ wget https://dl.dropboxusercontent.com/u/18038046/ora/oracle-instantclient12.1-sqlplus-12.1.0.2.0-1.x86_64.rpm
 fi
-
 sudo rpm -ivh $EXTRAS/jdk-8u111-linux-x64.rpm
 sudo rpm -ivh $EXTRAS/oracle-instantclient12.1-basic-12.1.0.2.0-1.x86_64.rpm
 sudo rpm -ivh $EXTRAS/oracle-instantclient12.1-devel-12.1.0.2.0-1.x86_64.rpm
